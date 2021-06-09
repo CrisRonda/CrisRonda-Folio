39477700772fb7b5b81db0893b5462ea8473a640
Cristian Ronda <cristian.j005@gmail.com>
2021-06-08 23:48:38 -0500

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,28 @@ import AudioPlayer from "src/components/AudioPlayer";
 import NavBar from "src/nav";
 import Footer from "src/components/Footer";
 import SEO from "src/components/SEO";
+import { useEffect } from "react";
+import * as ga from "analytics/index";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { statusCode } = pageProps;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <NextIntlProvider
