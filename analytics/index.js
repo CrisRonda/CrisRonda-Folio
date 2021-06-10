6b49af990dfa915115b76a5985cebd2db9764c39
Cristian Ronda <cristian.j005@gmail.com>
2021-06-10 13:30:56 -0500

    fix date in analytics

--- analytics/index.js
+++ analytics/index.js
@@ -1,6 +1,6 @@
 // log the pageview with their URL
 export const pageview = (url) => {
-  console.log("analytics...");
+  window.gtag("js", new Date());
   window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
     page_path: url,
   });
